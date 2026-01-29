41e552bc6b05e778dc7878447231a138bb714345
yael-bar <elbarakayassin37@gmail.com>
2026-01-29 01:34:21 +0100

    first commit

---
+++ More_or_less.js
@@ -0,0 +1,22 @@
+function more(n)
+{
+    return n + 1;
+}
+
+function less(n)
+{
+    return n - 1;
+}
+function add(n1, n2)
+{
+    return n1 + n2;
+}
+
+function sub(n1, n2)
+{
+    return n1 - n2;
+}
+console.log("more(5): " + more(5));
+console.log("less(5): " + less(5));
+console.log("add(5, 3): " + add(5, 3));
+console.log("sub(5, 3): " + sub(5, 3));
