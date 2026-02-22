07a72275c7406e9b189bdd1eaeeb76bb449d525e
yael-bar <elbarakayassin37@gmail.com>
2026-02-22 00:59:56 +0000

    slice

---
+++ keep_cut.js
@@ -0,0 +1,28 @@
+function cutFirst(str)
+{
+    return str.slice(2);
+}
+function cutLast(str)
+{
+    return str.slice(0, -2);
+}
+function cutFirstLast(str)
+{
+    return str.slice(2, -2);
+}
+function keepFirst(str)
+{
+    return str.slice(0, 2);
+}
+function keepLast(str)
+{
+    return str.slice(-2);
+}
+
+function keepFirstLast(str)
+{
+    let f_str = str.slice(0, 2);
+    return (f_str + str.slice(str.length - 2)); 
+}
+
+console.log(keepFirstLast("heabclo"));
